ba7c08ae6ae6edb961957bfe0c49eccb83314979
Chuck Burgess <cburgess@progressrail.com>
2012-11-28 15:27:21 -0600

    rest of the CollectionTest cases;

--- tests/unit/phpDocumentor/Fileset/CollectionTest.php
+++ tests/unit/phpDocumentor/Fileset/CollectionTest.php
@@ -49,6 +49,15 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         ;
     }
 
+    /**
+     * Get a count of the files that exist in the test suite's "data" directory.
+     * @return int
+     */
+    protected function getCountOfDataDirFiles()
+    {
+        return 2;
+    }
+
     /* __construct() ***************************************************/
 
     /** @covers \phpDocumentor\Fileset\Collection::__construct() */
@@ -187,7 +196,55 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         $this->fixture->addAllowedExtension('php3');
         $this->assertTrue(true, 'Test passed');
     }
+
     /* addDirectories() ***************************************************/
+
+    /** @covers \phpDocumentor\Fileset\Collection::addDirectories() */
+    public function testAddDirectoriesWhenGivenEmptyArrayDoesNothing()
+    {
+        $this->fixture->addDirectories(array());
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addDirectories() */
+    public function testAddDirectoriesWhenGivenOnePathSucceeds()
+    {
+        $this->fixture->addDirectories(array($this->getNameOfDataDir()));
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addDirectories() */
+    public function testAddDirectoriesWhenGivenTheSamePathTwiceIsOk()
+    {
+        $this->fixture->addDirectories(array($this->getNameOfDataDir()));
+        $this->fixture->addDirectories(array($this->getNameOfDataDir()));
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addDirectories() */
+    public function testAddDirectoriesWhenGivenManyPathsSucceeds()
+    {
+        $this->fixture->addDirectories(
+            array(
+                $this->getNameOfDataDir(),
+                $this->getNameOfDataDir() . '.',
+                $this->getNameOfDataDir() . '..',
+        ));
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addDirectories() */
+    public function testAddDirectoriesWhenGivenTheSamePathTwiceDoesNotResultInDuplicatedFilenames()
+    {
+        $this->fixture->setAllowedExtensions(array('phar', 'txt'));
+
+        $this->fixture->addDirectories(array($this->getNameOfDataDir()));
+        $this->fixture->addDirectories(array($this->getNameOfDataDir()));
+
+        $files = $this->fixture->getFilenames();
+
+        $this->assertEquals($this->getCountOfDataDirFiles(), count($files));
+    }
 
     /* addDirectory() ***************************************************/
 
@@ -294,9 +351,124 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    /** @covers \phpDocumentor\Fileset\Collection::addDirectory() */
+    public function testAddDirectoryWhenFollowSymlinksIsSet()
+    {
+        /*
+         * NOTE:
+         *     this test does not verify that any symlinks were followed...
+         *     it simply exercises the $finder->followLinks() line.
+         */
+        $this->fixture->setFollowSymlinks(true);
+
+        // load the data test folder... must add non-default extensions first
+        $this->fixture->setAllowedExtensions(array('phar', 'txt'));
+
+        $this->fixture->getIgnorePatterns()->append('(phar)');
+
+        $this->fixture->addDirectory($this->getNameOfDataDir());
+        $files = $this->fixture->getFilenames();
+
+        // this file should have been seen
+        $this->assertContains(
+            realpath($this->getNameOfDataDir() . 'fileWithText.txt'),
+            $files
+        );
+
+        // this file should have been ignored
+        $this->assertNotContains(
+            realpath($this->getNameOfDataDir() . 'test.phar'),
+            $files
+        );
+    }
+
     /* addFiles() ***************************************************/
 
+    /** @covers \phpDocumentor\Fileset\Collection::addFiles() */
+    public function testAddFilesWhenGivenEmptyArrayDoesNothing()
+    {
+        $this->fixture->addFiles(array());
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addFiles() */
+    public function testAddFilesWhenGivenOnePathSucceeds()
+    {
+        $this->fixture->addFiles(array($this->getNameOfDataDir() . 'test.phar'));
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addFiles() */
+    public function testAddFilesWhenGivenTheSamePathTwiceIsOk()
+    {
+        $this->fixture->addFiles(array($this->getNameOfDataDir() . 'test.phar'));
+        $this->fixture->addFiles(array($this->getNameOfDataDir() . 'test.phar'));
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addFiles() */
+    public function testAddFilesWhenGivenManyPathsSucceeds()
+    {
+        $this->fixture->addFiles(
+            array(
+                $this->getNameOfDataDir() . 'test.phar',
+                $this->getNameOfDataDir() . 'fileWithText.txt',
+            ));
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addFiles() */
+    public function testAddFilesWhenGivenTheSamePathTwiceDoesNotResultInDuplicatedFilenames()
+    {
+        $this->fixture->setAllowedExtensions(array('phar', 'txt'));
+
+        $this->fixture->addFiles(array($this->getNameOfDataDir() . 'test.phar'));
+        $this->fixture->addFiles(array($this->getNameOfDataDir() . 'test.phar'));
+
+        $files = $this->fixture->getFilenames();
+
+        $this->assertEquals(1, count($files));
+    }
+
     /* addFile() ***************************************************/
+
+    /** @covers \phpDocumentor\Fileset\Collection::addFile() */
+    public function testAddFileWhenGivenEmptyStringThrowsException()
+    {
+        $this->setExpectedException(
+            '\InvalidArgumentException',
+            'Expected filename or object of type SplFileInfo but received nothing at all'
+        );
+        $this->fixture->addFile('');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addFile() */
+    public function testAddFileWhenGivenOnePathSucceeds()
+    {
+        $this->fixture->addFile($this->getNameOfDataDir() . 'test.phar');
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addFile() */
+    public function testAddFileWhenGivenTheSamePathTwiceIsOk()
+    {
+        $this->fixture->addFile($this->getNameOfDataDir() . 'test.phar');
+        $this->fixture->addFile($this->getNameOfDataDir() . 'test.phar');
+        $this->assertTrue(true, 'Test passes');
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::addFile() */
+    public function testAddFileWhenGivenTheSamePathTwiceDoesNotResultInDuplicatedFilenames()
+    {
+        $this->fixture->setAllowedExtensions(array('phar', 'txt'));
+
+        $this->fixture->addFile($this->getNameOfDataDir() . 'test.phar');
+        $this->fixture->addFile($this->getNameOfDataDir() . 'test.phar');
+
+        $files = $this->fixture->getFilenames();
+
+        $this->assertEquals(1, count($files));
+    }
 
     /* getFilenames() ***************************************************/
 
@@ -386,12 +558,179 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
 
     /* getProjectRoot() ***************************************************/
 
+    /** @covers \phpDocumentor\Fileset\Collection::getProjectRoot() */
+    public function testGetProjectRootWhenNothingHasYetBeenAddedReturnsRootSlash()
+    {
+        $this->assertEquals(DIRECTORY_SEPARATOR, $this->fixture->getProjectRoot());
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::getProjectRoot() */
+    public function testGetProjectRootWhenOneDatafileWasAddedReturnsDataFolder()
+    {
+        $this->fixture->setAllowedExtensions(array('phar', 'txt'));
+        $this->fixture->addFile($this->getNameOfDataDir() . 'test.phar');
+
+        // realpath() steals our trailing directory separator
+        $expected = realpath($this->getNameOfDataDir()) . DIRECTORY_SEPARATOR;
+        $this->assertEquals($expected, $this->fixture->getProjectRoot());
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::getProjectRoot() */
+    public function testGetProjectRootWhenTwoDatafilesWereAddedReturnsDataFolder()
+    {
+        $this->fixture->setAllowedExtensions(array('phar', 'txt'));
+        $this->fixture->addFile($this->getNameOfDataDir() . 'test.phar');
+        $this->fixture->addFile($this->getNameOfDataDir() . 'fileWithText.txt');
+
+        // realpath() steals our trailing directory separator
+        $expected = realpath($this->getNameOfDataDir()) . DIRECTORY_SEPARATOR;
+        $this->assertEquals($expected, $this->fixture->getProjectRoot());
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::getProjectRoot() */
+    public function testGetProjectRootWhenTwoFilesAreApart()
+    {
+        $this->fixture->addFile(__FILE__);
+        $this->fixture->addFile($this->getNameOfDataDir() . 'fileWithText.txt');
+
+        // realpath() steals our trailing directory separator
+        $expected =
+            realpath(
+                $this->getNameOfDataDir()
+                . '..'
+            )
+            . DIRECTORY_SEPARATOR
+        ;
+        $this->assertEquals($expected, $this->fixture->getProjectRoot());
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::getProjectRoot() */
+    public function testGetProjectRootWhenTwoFilesAreVeryFarApart()
+    {
+        $this->fixture->addFile(__FILE__);
+        $this->fixture->addFile(
+            $this->getNameOfDataDir()
+            . '..' . DIRECTORY_SEPARATOR
+            . '..' . DIRECTORY_SEPARATOR
+            . 'src' . DIRECTORY_SEPARATOR
+            . 'phpDocumentor' . DIRECTORY_SEPARATOR
+            . 'Fileset' . DIRECTORY_SEPARATOR
+            . 'Collection.php'
+        );
+
+        // realpath() steals our trailing directory separator
+        $expected =
+        realpath(
+            $this->getNameOfDataDir()
+            . '..' . DIRECTORY_SEPARATOR
+            . '..' . DIRECTORY_SEPARATOR
+        )
+        . DIRECTORY_SEPARATOR
+        ;
+        $this->assertEquals($expected, $this->fixture->getProjectRoot());
+    }
+
     /* setIgnoreHidden() ***************************************************/
+
+    /** @covers \phpDocumentor\Fileset\Collection::setIgnoreHidden() */
+    public function testSetIgnoreHiddenGivenFalse()
+    {
+        /*
+         * NOTE:
+         *     this does not verify that hidden files were ignored...
+         *     it simply exercises the setIgnoreHidden() method.
+         */
+        $this->fixture->setIgnoreHidden(false);
+        $this->assertFalse($this->fixture->getIgnoreHidden());
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::setIgnoreHidden() */
+    public function testSetIgnoreHiddenGivenTrue()
+    {
+        /*
+         * NOTE:
+         *     this does not verify that hidden files were not ignored...
+         *     it simply exercises the setIgnoreHidden() method.
+         */
+        $this->fixture->setIgnoreHidden(true);
+        $this->assertTrue($this->fixture->getIgnoreHidden());
+    }
 
     /* getIgnoreHidden() ***************************************************/
 
+    /** @covers \phpDocumentor\Fileset\Collection::getIgnoreHidden() */
+    public function testGetIgnoreHiddenGivenFalse()
+    {
+        /*
+         * NOTE:
+         *     this does not verify that hidden files were ignored...
+         *     it simply exercises the getIgnoreHidden() method.
+         */
+        $this->fixture->setIgnoreHidden(false);
+        $this->assertFalse($this->fixture->getIgnoreHidden());
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::getIgnoreHidden() */
+    public function testGetIgnoreHiddenGivenTrue()
+    {
+        /*
+         * NOTE:
+         *     this does not verify that hidden files were not ignored...
+         *     it simply exercises the getIgnoreHidden() method.
+         */
+        $this->fixture->setIgnoreHidden(true);
+        $this->assertTrue($this->fixture->getIgnoreHidden());
+    }
+
     /* setFollowSymlinks() ***************************************************/
+
+    /** @covers \phpDocumentor\Fileset\Collection::setFollowSymlinks() */
+    public function testSetFollowSymlinksGivenFalse()
+    {
+        /*
+         * NOTE:
+        *     this does not verify that symlinks were not followed...
+        *     it simply exercises the setFollowSymlinks() method.
+        */
+        $this->fixture->setFollowSymlinks(false);
+        $this->assertFalse($this->fixture->getFollowSymlinks());
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::setFollowSymlinks() */
+    public function testSetFollowSymlinksGivenTrue()
+    {
+        /*
+         * NOTE:
+        *     this does not verify that symlinks were followed...
+        *     it simply exercises the setFollowSymlinks() method.
+        */
+        $this->fixture->setFollowSymlinks(true);
+        $this->assertTrue($this->fixture->getFollowSymlinks());
+    }
 
     /* getFollowSymlinks() ***************************************************/
 
+    /** @covers \phpDocumentor\Fileset\Collection::getFollowSymlinks() */
+    public function testGetFollowSymlinksGivenFalse()
+    {
+        /*
+         * NOTE:
+         *     this does not verify that symlinks were not followed...
+         *     it simply exercises the getFollowSymlinks() method.
+         */
+        $this->fixture->setFollowSymlinks(false);
+        $this->assertFalse($this->fixture->getFollowSymlinks());
+    }
+
+    /** @covers \phpDocumentor\Fileset\Collection::getFollowSymlinks() */
+    public function testGetFollowSymlinksGivenTrue()
+    {
+        /*
+         * NOTE:
+         *     this does not verify that symlinks were followed...
+         *     it simply exercises the getFollowSymlinks() method.
+         */
+        $this->fixture->setFollowSymlinks(true);
+        $this->assertTrue($this->fixture->getFollowSymlinks());
+    }
 }
